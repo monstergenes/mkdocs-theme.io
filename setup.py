c958e7bcc9d23598630a8916514ef90cebf15787
MONSTERCRYST <maresjuan2019@outlook.com>
2023-12-23 13:38:24 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,23 @@ VERSION = '1.1'
 
 
 setup(
-    name="munstergenes-mkdocs-theme",
+    name="monstergenes/mkdocs-theme.io",
     version=VERSION,
-    url='https://github.com/munstergenes/munstergenes-mkdocs-theme',
+    url='https://github.com/monstergenes/mkdocs-theme.io/',
     license='MIT',
     description='dark video background header and sleek',
     author='MONSTERCRYST',
-    author_email='https://github.com/monstergenes',
+    author_email='munsterkreations@icloud.com',
     packages=find_packages(),
     include_package_data=True,
 
     entry_points={
-        'mkdocs.themes': [
-            'munstergenestheme = munstergenes-mkdocs-theme',
+        'custom_themes': [
+            'Home =  main.html',
+            'Stylesheet = styles.css',
             ]
+        'docs':
+            'about = about.md',
+            'index = index.html',
           },
          zip_safe=False)
